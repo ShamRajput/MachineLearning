d7411c41eba5e4c23b4a610eeb335406278b86f8
shamkumar <shamkumar.rajput@hotmail.com>
2024-01-09 04:54:21 -0500

    PP:Calculator and Madlibs

---
+++ python/MadLibs.py
@@ -0,0 +1,9 @@
+
+name = input("Enter a child name: ")
+item = input ("Enter a eatable item: ")
+
+
+print( name +" "+ name + " yes pass")
+print("Eating " + item + " no papa")
+print("telling lies no papa")
+print("open your mouth, HaHaHa...")
