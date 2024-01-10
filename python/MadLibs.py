b71868b3337f3fdeaada97a0b6ecab9bfc2cd438
shamkumar <shamkumar.rajput@hotmail.com>
2024-01-10 03:49:02 -0500

    PP: Guessing Game

--- python/MadLibs.py
+++ python/MadLibs.py
@@ -3,7 +3,7 @@ name = input("Enter a child name: ")
 item = input ("Enter a eatable item: ")
 
 
-print( name +" "+ name + " yes pass")
+print( name +" "+name + " yes pass")
 print("Eating " + item + " no papa")
-print("telling lies no papa")
-print("open your mouth, HaHaHa...")
+print("Telling lies no papa")
+print("Open your mouth, HaHaHa...")
