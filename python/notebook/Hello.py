dc3b5813ebbf29302c8ffd6210cccfb0e4e885f1
shamkumar <shamkumar.rajput@hotmail.com>
2024-01-10 10:03:02 -0500

    PP:Notebook related comment added in the code for future reference

--- python/notebook/Hello.py
+++ python/notebook/Hello.py
@@ -1,3 +1,6 @@
+#Basically to create jypter  notebook locally we require ipkernel and anaconda
+#after installation of anaconda and kernel and jypter extension we can run jypter notebook through vscode.
+
 # %%
 var =10
 print(var)
